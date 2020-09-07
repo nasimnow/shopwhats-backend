6f148cc3f92cbbd1d084b1ccf87fc73dbc8789b4
muhammednasimda <muhammednasimda@gmail.com>
2020-09-07 15:30:54 +0530

    Think Fixed My SQL

--- connection.js
+++ connection.js
@@ -1,17 +1,11 @@
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
     host: "us-cdbr-east-02.cleardb.com",
     user: "bf04310140ea86",
     password: "bdd503d0",
     database: "heroku_377dc08c48ea374",
     multipleStatements: true
 })
-mysqlConnection.connect((err)=>{
-    if(!err){
-        console.log("Connected");
-    }else{
-        console.log('fAILED')
-    }
-})
+
 module.exports = mysqlConnection;
