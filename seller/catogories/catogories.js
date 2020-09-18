245a77aebfc2473c937ecb62a2fe46135198a7d0
muhammednasimda <muhammednasimda@gmail.com>
2020-09-18 13:01:22 +0530

    images testing

--- seller/catogories/catogories.js
+++ seller/catogories/catogories.js
@@ -6,43 +6,57 @@ const router = express.Router();
 router.get('/',(req,res)=>{
     let sql = `SELECT *FROM catogories WHERE cat_user=${req.user[0].id}`
     let query = mysqlConnection.query(sql,(err, results)=>{
-        if(err) return res.status(500).json({message:{messageBody :err,status: false}})
-        return res.status(201).json({results})
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(200).json({status:true,login:true,data:results})
     })
 })
-
+//get all parent catogories
+router.get('/parent',(req,res)=>{
+    let sql = `SELECT *FROM catogories_main`
+    let query = mysqlConnection.query(sql,(err, results)=>{
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(200).json({status:true,login:true,data:results})
+    })
+})
+//get parent catogory by id
+router.get('/parent/:id',(req,res)=>{
+    let sql = `SELECT *FROM catogories_main WHERE id=${req.params.id}`
+    let query = mysqlConnection.query(sql,(err, results)=>{
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(200).json({status:true,login:true,data:results})
+    })
+})
 //get specific catogories by id
 router.get('/:id',(req,res)=>{
     let sql = `SELECT *FROM catogories WHERE id=${req.params.id} AND cat_user=${req.user[0].id}`
     let query = mysqlConnection.query(sql,(err, results)=>{
-        if(err) return res.status(500).json({message:{messageBody :err,status: false}})
-        return res.status(201).json({results})
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(200).json({status:true,login:true,data:results})
     })
 })
 
 router.delete('/:id',(req,res)=>{
     let sql = `DELETE FROM catogories  WHERE id =${req.params.id} AND cat_user =${req.user[0].id} `
     let query = mysqlConnection.query(sql,(err, results)=>{
-        if(err) return res.status(500).json({message:{messageBody :err,status: false}})
-        res.status(201).json({message:{messageBody :`Succesfully Deleted `,status: true,login: true}})
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(201).json({status:true,login:true,data:{id:req.params.id}}) 
     })
 })
 router.post('/',(req,res)=>{
-    let cat ={cat_name:req.body.cat_name,cat_user:req.user[0].id}
+    let cat ={cat_name:req.body.cat_name,cat_parent:req.body.cat_parent,cat_user:req.user[0].id}
     let sql = 'INSERT INTO catogories SET ?'
     let query = mysqlConnection.query(sql,cat,(err, result)=>{
-        if(err) return res.status(500).json({message:{messageBody :err,status: false}})
-        res.status(201).json({message:{messageBody :`Succesfully Added ${req.body.cat_name}`,status: true,login: true}})
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(201).json({status:true,login:true,data:cat})
     })
 })
 
 router.put('/',(req,res)=>{
-    let cat ={cat_name:req.body.cat_name}
+    let cat ={cat_name:req.body.cat_name,cat_parent:req.body.cat_parent}
     let sql = `UPDATE catogories SET ? WHERE id=${req.body.id}`
     let query = mysqlConnection.query(sql,cat,(err, result)=>{
-        if(err) return res.status(500).json({message:{messageBody :err,status: false}})
-        res.status(201).json({message:{messageBody :`Succesfully Updated ${req.body.cat_name}`,status: true,login: true}})
-    })
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(201).json({status:true,login:true,data:cat})    })
 })
 
 module.exports = router
--- seller/products/products.js
+++ seller/products/products.js
@@ -4,7 +4,13 @@ const router = express.Router();
 
 //get all products of current user
 router.get('/',(req,res)=>{
-    let sql = `SELECT *FROM products WHERE product_user=${req.user[0].id}`
+    let sql=`SELECT  products.* , GROUP_CONCAT(product_image ORDER BY products_images.id) AS images
+    FROM    products 
+    LEFT JOIN    products_images
+    ON      products_images.product_id = products.id
+    WHERE product_user=${req.user[0].id}
+    GROUP BY products.id`
+    //let sql = `SELECT *FROM products WHERE product_user=${req.user[0].id}`
     let query = mysqlConnection.query(sql,(err, results)=>{
         if(err) return res.status(500).json({message:{messageBody :err,status: false}})
         return res.status(200).json({status:true,login:true,data:results})
@@ -13,7 +19,7 @@ router.get('/',(req,res)=>{
 
 //get specific product
 router.get('/:id',(req,res)=>{
-    let sql = `SELECT *FROM products WHERE product_id =${req.params.id} AND product_user =${req.user[0].id}`
+    let sql = `SELECT *FROM products WHERE id =${req.params.id} AND product_user =${req.user[0].id}`
     let query = mysqlConnection.query(sql,(err, results)=>{
         if(err) return res.status(500).json({status:false,error:{message :err}})
         return res.status(200).json({status:true,login:true,data:results})
@@ -26,13 +32,14 @@ router.post('/',(req,res)=>{
     let sql = 'INSERT INTO products SET ?'
     let query = mysqlConnection.query(sql,product,(err, result)=>{
         if(err) return res.status(500).json({status:false,error:{message :err}})
-        return res.status(201).json({status:true,login:true,data:product})})
+        return res.status(201).json({status:true,login:true,data:product})
+    })
 })
 
 //update specific product
 router.put('/',(req,res)=>{
     let product ={product_name:req.body.product_name,product_price:req.body.product_price,product_desc:req.body.product_desc,product_stock:req.body.product_stock,product_cat:req.body.product_cat}
-    let sql = `UPDATE products  SET ? WHERE product_id =${req.body.product_id} AND product_user =${req.user[0].id} `
+    let sql = `UPDATE products  SET ? WHERE id =${req.body.id} AND product_user =${req.user[0].id} `
     let query = mysqlConnection.query(sql,product,(err, result)=>{
         if(err) return res.status(500).json({status:false,error:{message :err}})
         return res.status(201).json({status:true,login:true,data:product})
@@ -41,15 +48,16 @@ router.put('/',(req,res)=>{
 
 //delete specific product
 router.delete('/:id',(req,res)=>{
-    let sql = `DELETE FROM products  WHERE product_id =${req.params.id} AND product_user =${req.user[0].id} `
+    let sql = `DELETE FROM products  WHERE id =${req.params.id} AND product_user =${req.user[0].id} `
     let query = mysqlConnection.query(sql,(err, result)=>{
         if(err) return res.status(500).json({status:false,error:{message :err}})
-        return res.status(201).json({status:true,login:true,data:{id:req.params.id}})    })
+        return res.status(201).json({status:true,login:true,data:{id:req.params.id}})    
+    })
 })
 
 //flip product stock status
 router.put('/stock/:id',(req,res)=>{
-    let sql = `UPDATE products  SET product_stock = NOT product_stock WHERE product_id=${req.params.id} AND product_user =${req.user[0].id}`
+    let sql = `UPDATE products  SET product_stock = NOT product_stock WHERE id=${req.params.id} AND product_user =${req.user[0].id}`
     let query = mysqlConnection.query(sql,(err, result)=>{
         if(err) return res.status(500).json({status:false,error:{message :err}})
         return res.status(201).json({status:true,login:true,data:{id:req.params.id}})    
@@ -65,4 +73,15 @@ router.get('/catogories/:cat',(req,res)=>{
         return res.status(200).json({status:true,login:true,data:results})
     })
 })
+
+//get no of products under a catogory
+router.get('/catogories/no/:cat',(req,res)=>{
+   
+    let sql = `SELECT COUNT(*) AS count FROM products WHERE product_cat =${req.params.cat} AND product_user =${req.user[0].id}`
+    let query = mysqlConnection.query(sql,(err, results)=>{
+        if(err) return res.status(500).json({status:false,error:{message :err}})
+        return res.status(200).json({status:true,login:true,data:{products_count:results[0].count}})
+    })
+})
+
 module.exports = router
