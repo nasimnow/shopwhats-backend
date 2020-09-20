052526f4d326658f6d95fd774f90737f760bb457
muhammednasimda <muhammednasimda@gmail.com>
2020-09-20 09:23:37 +0530

    new with extra cors

--- app.js
+++ app.js
@@ -1,114 +1,38 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-const express = require("express");
-const passport = require("passport");
-const { v4: uuidv4 } = require("uuid");
-const session = require("express-session");
-const flash = require("express-flash");
+const util = require("util");
 const fs = require("fs");
-const cors = require("cors");
-const mysqlConnection = require("./connection");
-const FileStore = require("session-file-store")(session);
+const path = require("path");
+const express = require("express");
+var cors = require("cors");
+const readDir = util.promisify(fs.readdir);
+var multer = require("multer");
+var upload = multer({ dest: "./images" });
+
+async function getImageList(dir) {
+  try {
+    return await readDir(path.join(__dirname, "public", dir));
+  } catch (error) {
+    throw error;
+  }
+}
 
 const app = express();
-app.options("*", cors());
 
-const PORT = process.env.PORT || 3000;
+app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested- With, Content-Type, Accept"
-  );
-  next();
+/* This is the endpoint that handles the upload. FilePond expects a unique id returned
+   so it can request an undo if needed. The filename prop works well for this. */
+app.post("/seller/products/uploadimage", upload.array("images", 12), function (
+  req,
+  res,
+  next
+) {
+  // req.files is array of `photos` files
+  console.log(req.files);
+  // req.body will contain the text fields, if there were any
+  console.log(req.body);
+  res.send([req.files[0].filename]);
 });
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
-require("./seller/account/passport-config")(passport);
-app.set("view-engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  session({
-    genid: (req) => {
-      return uuidv4();
-    },
-    store: new FileStore(),
-    cookie: { maxAge: 172800000 },
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/seller/register", isNotLogin, require("./seller/account/register"));
-
-app.use("/seller/login", isNotLogin, require("./seller/account/login"));
-
-app.use("/seller/products", require("./seller/products/products"));
-
-app.use(
-  "/seller/catogories",
-  isLogin,
-  require("./seller/catogories/catogories")
-);
-
-app.use("/seller/store", isLogin, require("./seller/account/store"));
-
-app.get("/seller/loginstatus", (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.status(201).json({
-      message: { messageBody: "Loginned", status: true, login: true },
-    });
-  }
-  return res.status(500).json({
-    message: { messageBody: "Not Loginned", status: false, login: false },
-  });
-});
-
-app.get("/seller/logout", isLogin, (req, res) => {
-  req.logOut();
-  res.json({ message: "succesfull Logggedout", login: false });
-});
-
-app.get("/upload", (req, res) => {
-  res.render("upload.ejs");
-});
-
-function isLogin(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  return res.status(401).json({
-    status: false,
-    login: false,
-    error: { message: "Please Login", code: 103 },
-  });
-}
-function isNotLogin(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.status(400).json({
-      status: false,
-      login: true,
-      error: { message: "Already Loginned", code: 104 },
-    });
-  }
-  return next();
-}
-
-app.listen(PORT);
+app.listen(3001, () => console.log("Example app listening on port 3001!"));
