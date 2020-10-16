5010958c981d6a77100ca1a7a79d7bfe53fc2c01
muhammednasimda <muhammednasimda@gmail.com>
2020-10-16 23:13:00 +0530

    testing

--- seller/account/login.js
+++ seller/account/login.js
@@ -30,11 +30,13 @@ const jwt = require("jsonwebtoken");
 router.post("/", (req, res) => {
   let query = `SELECT * FROM account WHERE account_phone ='${req.body.phone}'`;
   mysqlConnection.query(query, async (err, user) => {
-    if (err) {
+    if (err){ return res
+    .status(500)
+    .json({ status_code: 500, status: false, error: { message: err } });
     }
-   
+    if(user!=null){
     user = user[0];
-
+    console.log(user)
     if (user != "") {
       if (await bcrypt.compare(req.body.password, user.account_password)) {
         console.log("settt");
@@ -61,6 +63,8 @@ router.post("/", (req, res) => {
         error: { message: "Check your credentials", code: 101 },
       });
     }
+  }
   });
+
 });
 module.exports = router;
