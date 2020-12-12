81eb789cbfe1c7b88c6ee4f608423c288e45fb0e
muhammednasimda <muhammednasimda@gmail.com>
2020-12-12 16:06:33 +0530

    cors error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 var cookieParser = require("cookie-parser");
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 //cors plugin not working
 app.use(function (req, res, next) {
   res.header(
