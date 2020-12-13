be09f5ebd230ae4a54183b11869b2c624fb0399d
muhammednasimda <muhammednasimda@gmail.com>
2020-12-13 14:33:24 +0530

    added cors support for put

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 //cors plugin not working
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   next();
 });
 
