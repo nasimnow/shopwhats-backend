49aaf814ead842751ba21ec839e3dfa167e9c5bc
muhammednasimda <muhammednasimda@gmail.com>
2020-12-29 22:09:50 +0530

    localhost error kinda fixe

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.use(function (req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept,Authorization"
   );
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-
   next();
 });
 
