0be8d8fc916bba68c9a55c5ef7a29aacb7fd1920
muhammednasimda <muhammednasimda@gmail.com>
2020-12-30 21:19:27 +0530

    added get prodcts by category

--- client/store.js
+++ client/store.js
@@ -53,26 +53,38 @@ router.get("/products/single/:id", (req, res) => {
 });
 
 //get all instock products of user
-router.get("/products/:id", (req, res) => {
-  let sql = `SELECT  products.* , GROUP_CONCAT(product_image ORDER BY products_images.id) AS images
+router.get("/products/:id/:cat", (req, res) => {
+  let sqlAll = `SELECT  products.* , GROUP_CONCAT(product_image ORDER BY products_images.id) AS images
     FROM    products 
     LEFT JOIN    products_images
     ON      products_images.product_id = products.id
     WHERE product_user='${req.params.id}' AND product_stock !=0
     GROUP BY products.id`;
-  let query = mysqlConnection.query(sql, (err, results) => {
-    if (err)
+
+  //filter products by category if need
+  let sqlCat = `SELECT  products.* , GROUP_CONCAT(product_image ORDER BY products_images.id) AS images
+    FROM    products 
+    LEFT JOIN    products_images
+    ON      products_images.product_id = products.id
+    WHERE products.product_cat =${req.params.cat} AND product_stock !=0 AND WHERE product_user='${req.params.id}'
+    GROUP BY products.id`;
+
+  let query = mysqlConnection.query(
+    req.params.cat == "all" ? sqlAll : sqlCat,
+    (err, results) => {
+      if (err)
+        return res.json({
+          status_code: 500,
+          message: { messageBody: err, status: false },
+        });
       return res.json({
-        status_code: 500,
-        message: { messageBody: err, status: false },
+        status_code: 200,
+        status: true,
+        login: true,
+        data: results,
       });
-    return res.json({
-      status_code: 200,
-      status: true,
-      login: true,
-      data: results,
-    });
-  });
+    }
+  );
 });
 
 module.exports = router;
