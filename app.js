9d94c6023f564f4e30f77c0cacc52199b1d04871
muhammednasimda <muhammednasimda@gmail.com>
2021-01-02 11:42:17 +0530

    analytics return edit

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-var cookieParser = require("cookie-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 //cors plugin not working
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+//   next();
+// });
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
