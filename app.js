a92f0f717b8d672c3940daa46cc91ee8dc602d5c
muhammednasimda <muhammednasimda@gmail.com>
2021-01-02 11:35:19 +0530

    analytics return edit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 //cors plugin not working
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept,Authorization"
