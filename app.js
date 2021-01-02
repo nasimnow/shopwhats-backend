a2b1736f7acc00fb842942458bed032b3f2c5725
muhammednasimda <muhammednasimda@gmail.com>
2021-01-02 11:55:09 +0530

    analytics return edit

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header(
     "Access-Control-Allow-Methods",
     "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept,Authorization"
   );
   next();
 });
