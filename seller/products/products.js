f9b7c8789c18e06a3c73ee12270d5151f431ff1a
muhammednasimda <muhammednasimda@gmail.com>
2021-01-02 12:53:41 +0530

    analytics return edit

--- seller/products/products.js
+++ seller/products/products.js
@@ -33,7 +33,7 @@ router.get("/", (req, res) => {
 //get count of all products of a user
 router.get("/count", (req, res) => {
   let sql = `select (select count(*) from products WHERE product_user=${req.user.user.id}) as products_count  ,
-  (select count(*) from catogories WHERE cat_user=${req.user.user.id}) as cat_count ,(select store_views,message_clicks from store_analytics WHERE user_id=${req.user.user.id}) `;
+  (select count(*) from catogories WHERE cat_user=${req.user.user.id}) as cat_count ,(select store_views from store_analytics WHERE user_id=${req.user.user.id}) as store_views,(select message_clicks from store_analytics WHERE user_id=${req.user.user.id}) as message_clicks`;
 
   let query = mysqlConnection.query(sql, (err, results) => {
     if (err)
