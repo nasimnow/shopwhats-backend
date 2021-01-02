77be56170645f7c4b91052f88314ae3e92473563
muhammednasimda <muhammednasimda@gmail.com>
2021-01-02 13:21:28 +0530

    analytics return edit

--- seller/products/products.js
+++ seller/products/products.js
@@ -35,7 +35,7 @@ router.get("/", (req, res) => {
 router.get("/count", (req, res) => {
   const today = moment().format("YYYY-MM-DD");
   let sql = `select (select count(*) from products WHERE product_user=${req.user.user.id}) as products_count  ,
-  (select count(*) from catogories WHERE cat_user=${req.user.user.id}) as cat_count ,(select store_views from store_analytics WHERE user_id=${req.user.user.id} AND date="2021-01-03") as store_views,(select message_clicks from store_analytics WHERE user_id=${req.user.user.id}) as message_clicks`;
+  (select count(*) from catogories WHERE cat_user=${req.user.user.id}) as cat_count ,(select store_views from store_analytics WHERE user_id=${req.user.user.id} AND date=${today}) as store_views,(select message_clicks from store_analytics WHERE user_id=${req.user.user.id} AND date=${today}) as message_clicks`;
 
   let query = mysqlConnection.query(sql, (err, results) => {
     if (err)
