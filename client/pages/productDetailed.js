b2c307b662a5ce36dd9fa792248812ce3347098e
muhammednasimda <muhammednasimda@gmail.com>
2021-01-04 16:53:28 +0530

    added similar proucts to product detailed api

--- client/pages/productDetailed.js
+++ client/pages/productDetailed.js
@@ -16,17 +16,27 @@ router.get("/:productId", (req, res) => {
         .status(404)
         .json({ status: false, error: "Product Doesnt Exist" });
 
-    let sqlStoreDetails = `SELECT id,account_store,account_whatsapp,account_store_link,account_store_status,account_store_desc,account_store_address FROM account WHERE id = '${productDetails[0].product_user}'`;
-    mysqlConnection.query(sqlStoreDetails, (error, storeDetails) => {
-      if (error) return res.status(500).json({ status: false, error });
-      return res.status(200).json({
-        status: true,
-        data: {
-          storeinfo: storeDetails[0],
-          product: productDetails[0],
-        },
-      });
-    });
+    let sqlStoreDetailsProducts = `SELECT id,account_store,account_whatsapp,account_store_link,account_store_status,account_store_desc,account_store_address FROM account WHERE id = '${productDetails[0].product_user}';
+    SELECT  products.* , GROUP_CONCAT(product_image ORDER BY products_images.id) AS images
+    FROM    products
+    LEFT JOIN    products_images
+    ON      products_images.product_id = products.id
+    WHERE product_user='${productDetails[0].product_user}' AND product_stock !=0 AND products.id!= ${req.params.productId}  
+    GROUP BY products.id  LIMIT 4`;
+    mysqlConnection.query(
+      sqlStoreDetailsProducts,
+      (error, storeDetailsProducts) => {
+        if (error) return res.status(500).json({ status: false, error });
+        return res.status(200).json({
+          status: true,
+          data: {
+            storeinfo: storeDetailsProducts[0],
+            product: productDetails[0],
+            similarproducts: storeDetailsProducts[1],
+          },
+        });
+      }
+    );
   });
 });
 
