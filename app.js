98307a40e2a34ef0a4489c2f14c30a7c305f924b
muhammednasimda <muhammednasimda@gmail.com>
2021-01-11 08:43:41 +0530

    cors eror fixing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: req.header("origin") }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
