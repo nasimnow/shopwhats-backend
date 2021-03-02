f07cd40359a3dae222700c6315164d31d1531d5e
muhammednasimda <muhammednasimda@gmail.com>
2021-03-02 20:12:28 +0530

    cors error

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 let cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ credentials: true, origin: "*" }));
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
