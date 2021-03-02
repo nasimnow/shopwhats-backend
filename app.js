a43f57ac3504f727d7a429eaa5018e6a330b2a40
muhammednasimda <muhammednasimda@gmail.com>
2021-03-02 20:03:25 +0530

    cors error

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
