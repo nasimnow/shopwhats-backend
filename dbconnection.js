02c58cc54ee1bb9bed7c08dbc48a047220779e7c
muhammednasimda <muhammednasimda@gmail.com>
2021-03-07 19:26:02 +0530

    added mysql hardcoded

--- dbconnection.js
+++ dbconnection.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize("mysql://root:silverstar@localhost:3306/shopwhats");
 sequelize.options.logging = false;
 module.exports = sequelize;
