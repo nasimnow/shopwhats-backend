5d54d317f9299b607d9796e2c8c6f6bb78a09d00
muhammednasimda <muhammednasimda@gmail.com>
2021-03-07 17:46:14 +0530

    cors change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ credentials: true, origin: "https://freshostore.in" }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
