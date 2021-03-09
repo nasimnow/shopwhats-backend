b6f35b911ad845707a35a2a468d2c398d3be6cd1
muhammednasimda <muhammednasimda@gmail.com>
2021-03-09 19:57:58 +0530

    mysql change

--- dbconnection.js
+++ dbconnection.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("mysql://root:silverstar@localhost:3306/shopwhats");
+const sequelize = new Sequelize("mysql://root:silverstar@localhost:3306/fliqcwco_shopwhats");
 sequelize.options.logging = false;
 module.exports = sequelize;
