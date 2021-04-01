744c4994abe48b136ff267627f8aa4e450030631
muhammednasimda <muhammednasimda@gmail.com>
2021-04-01 22:17:29 +0530

    cors error fixing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:3000", "https://www.saav.in"] }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
