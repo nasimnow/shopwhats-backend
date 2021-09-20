14a8de60c504452c2217626d99e2e94fe35a0e28
muhammednasimda <muhammednasimda@gmail.com>
2021-09-20 13:07:02 +0530

    added no product api

--- client/pages/productDetailed.js
+++ client/pages/productDetailed.js
@@ -8,35 +8,42 @@ const Sequilize = require("sequelize");
 const Op = Sequilize.Op;
 
 router.get("/:productId", async (req, res) => {
-  const product = await models.products.findByPk(
-    parseInt(req.params.productId),
-    {
-      include: [
-        { model: models.products_images, as: "products_images" },
-        {
-          model: models.products_variants,
-          as: "products_variants",
-        },
-      ],
-    }
-  );
-  const storeinfo = await models.account.findByPk(product.product_user, {
-    attributes: {
-      exclude: ["account_password"],
-    },
-  });
-  const similarproducts = await models.products.findAll({
-    where: { product_user: storeinfo.id, product_stock: { [Op.ne]: 0 } },
-    limit: 5,
-  });
-  return res.status(200).json({
-    status: true,
-    data: {
-      storeinfo,
-      product,
-      similarproducts,
-    },
-  });
+  try {
+    const product = await models.products.findByPk(
+      parseInt(req.params.productId),
+      {
+        include: [
+          { model: models.products_images, as: "products_images" },
+          {
+            model: models.products_variants,
+            as: "products_variants",
+          },
+        ],
+      }
+    );
+    const storeinfo = await models.account.findByPk(product.product_user, {
+      attributes: {
+        exclude: ["account_password"],
+      },
+    });
+    const similarproducts = await models.products.findAll({
+      where: { product_user: storeinfo.id, product_stock: { [Op.ne]: 0 } },
+      limit: 5,
+    });
+    return res.status(200).json({
+      status: true,
+      data: {
+        storeinfo,
+        product,
+        similarproducts,
+      },
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: false,
+      error,
+    });
+  }
 });
 
 module.exports = router;
