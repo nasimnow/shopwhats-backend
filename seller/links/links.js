eba0a69fbe5bf821949e2c4ca71561eb40de6026
muhammednasimda <muhammednasimda@gmail.com>
2021-11-05 15:19:49 +0530

    updated alonks api

--- seller/links/links.js
+++ seller/links/links.js
@@ -6,26 +6,51 @@ const initModels = require("../../models/init-models");
 const models = initModels(sequelize);
 
 //add or edit links
-router.post("/", async (req, res) => {
+router.post("/reorder", async (req, res) => {
   const { links } = req.body;
   try {
     links.forEach(async (link) => {
       link = { ...link, account_id: req.user.user.id };
-      if (link.id) {
-        await models.links.update(link, { where: { id: link.id } });
-      } else {
-        await models.links.create(link);
-      }
+      if (link.id) await models.links.update(link, { where: { id: link.id } });
     });
-    const linksNew = await models.links.findAll({
-      where: {
-        account_id: req.user.user.id,
-      },
-    });
+
     res.status(200).send({
       success: true,
       message: "Links updated successfully",
-      data: linksNew,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.post("/add", async (req, res) => {
+  const { link } = req.body;
+  try {
+    link.account_id = req.user.user.id;
+    let resp = await models.links.create(link);
+    res.status(200).send({
+      success: true,
+      message: "Link added successfully",
+      data: resp,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.post("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await models.links.destroy({ where: { id } });
+    res.status(200).send({
+      success: true,
+      message: "Link deleted successfully",
     });
   } catch (error) {
     res.status(400).send({
