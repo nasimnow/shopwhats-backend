cad8ff94e5d1db0034c0fd70106a3343030cbb8b
hadi-a-azeez <hadikoduvally@gmail.com>
2022-04-17 21:41:44 +0530

    added links domain support

--- client/pages/linksFront.js
+++ client/pages/linksFront.js
@@ -7,10 +7,54 @@ const models = initModels(sequelize);
 const Sequilize = require("sequelize");
 const Op = Sequilize.Op;
 
-router.get("/:account_username", async (req, res) => {
+router.get("/link/:account_username", async (req, res) => {
   try {
     const account_info_raw = await models.account.findOne({
       where: { account_store_link: req.params.account_username },
+    });
+    if (!account_info_raw)
+      return res
+        .status(404)
+        .json({ status: false, error: "User Doesnt Exist" });
+
+    const {
+      account_password,
+      account_last_login,
+      account_register_date,
+      account_notif_token,
+      ...account_info
+    } = account_info_raw.dataValues;
+
+    const user_settings = await models.settings.findOne({
+      where: {
+        user_id: account_info.id,
+      },
+    });
+
+    const links = await models.links.findAll({
+      where: {
+        account_id: account_info.id,
+      },
+      order: [["position", "asc"]],
+    });
+    res.json({
+      success: true,
+      account_info,
+      links,
+      user_settings,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Get failed",
+    });
+  }
+});
+
+router.get("/domain/:account_domain", async (req, res) => {
+  try {
+    const account_info_raw = await models.account.findOne({
+      where: { account_domain: req.params.account_domain },
     });
     if (!account_info_raw)
       return res
